a1c62947e63117749d64fa2cbf2ce6e088f3cee4
Robson Bettini <robson.bettini@hotmail.com>
2022-08-11 21:53:01 -0300

    Implementation to recover data

--- jdbc2/src/db/DB.java
+++ jdbc2/src/db/DB.java
@@ -4,7 +4,9 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
@@ -36,11 +38,31 @@ public class DB {
 
     private static Properties loadProperties() {
         try (FileInputStream fs = new FileInputStream("db.properties")) {
-            Properties props = new Properties();
+            Properties props = new Properties(); 
             props.load(fs);
             return props;
         } catch (IOException e) {
             throw new DbException(e.getMessage());
         }
+    }
+    
+    public static void closeStatement(Statement st) {
+    	if (st != null) {
+    		try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+    	}
+    }
+    
+    public static void closeResultSet(ResultSet rs) {
+    	if (rs != null) {
+    		try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+    	}
     }
 }
